d8be20ed8f2beeaca304692d22bc6eea9e4c5feb
Bas Westerbaan <bas@westerbaan.name>
2014-02-21 13:59:20 +0100

    Polygon.contains

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,20 @@
 """ The unit sphere. """
 import math
+import random
+
+def sign(x):
+    """ Returns 0 if x is zero.  1 if x is strictly positive.  -1, otherwise,
+        when x is strictly negative. """
+    if x == 0:
+        return 0
+    if x >= 0:
+        return 1
+    return -1
+
+class Error(Exception):
+    pass
+class SameGreatCircle(Error):
+    pass
 
 class Polygon:
     """ A polygon on the sphere. """
@@ -10,8 +25,93 @@ class Polygon:
             is the short segment between vi and vj. """
         assert all([v != external_point for v in vertices])
         assert len(frozenset(vertices)) == len(vertices)
+        assert len(vertices) >= 3
         self.vertices = vertices
         self.external_point = external_point
+        self.segments = []
+        for i in xrange(len(self.vertices)):
+            if i == 0:
+                first_point = self.vertices[-1]
+            else:
+                first_point = self.vertices[i - 1]
+            self.segments.append(Segment(first_point, self.vertices[i]))
+
+    def contains(self, point):
+        """ Checks whether the polygon contains the given point. """
+        # TODO prevent iteration over every segment.  Bounding box?
+        # First check whether the point is on one of the segments.
+        for segment in self.segments:
+            if segment.contains(point):
+                return True
+        # Or is the fixed external point.
+        if point == self.external_point:
+            return False
+        # Now we consider the segment from the point to the fixed external
+        # point.  Then we count the proper intersections with the polygon.  If
+        # this count is odd, then the point is inside the polygon.  Otherwise
+        # it is outside.
+        if point != -self.external_point:
+            ray = Segment(point, self.external_point)
+        else:
+            # If the point and the external point are antipodal, then we
+            # pick any other point, check whether it is an external point
+            # and use it to determine the containment.
+            N = 2
+            while True:
+                N *= 2
+                other_point = Point(random.randint(1, N),
+                                    random.randint(0, N),
+                                    random.randint(0, N))
+                if other_point.collinear(self.external_point):
+                    continue
+                ok = True
+                for segment in self.segments:
+                    if segment.contains(other_point):
+                        ok = False
+                        break
+                if not ok:
+                    continue
+                break
+            other_point_is_external = not self.contains(other_point)
+            other_polygon = Polygon(self.vertices, other_point)
+            other_polygon_contains_point = other_polygon.contains(point)
+            if other_point_is_external:
+                return other_polygon_contains_point
+            return not other_polygon_contains_point
+        intersection_count = 0
+        for i, segment in enumerate(self.segments):
+            try:
+                point = ray.intersection(segment)
+            except SameGreatCircle:
+                continue
+            if point is None:
+                continue
+            if not segment.is_endpoint(point):
+                intersection_count += 1
+                continue
+            # The ray crosses and endpoint of the segment.  We need to check
+            # whether the polygon actually crosses the ray or bounces off.
+            # At least two segments are involved: the incoming, possibly
+            # some intermediate, and an outgoing.  We will consider this
+            # crossing at the incoming segment and ignore the others.
+            if segment.point2 != point:
+                continue
+            incoming_direction = sign(scalar_triple_product(ray.point1,
+                                    ray.point2, segment.point1))
+            j = (i + 1) % len(self.segments)
+            while True:
+                next_segment = self.segments[j]
+                if not ray.contains(next_segment.point2):
+                    outgoing_direction = sign(scalar_triple_product(ray.point1,
+                                            ray.point2, next_segment.point2))
+                    break
+                j = (i + 1) % len(self.segments)
+            # If the outgoing and incoming directions are the same, then
+            # this was not a proper crossing.  Ignore it.
+            if outgoing_direction == incoming_direction:
+                continue
+            intersection_count += 1
+        return intersection_count % 2 != 0
     def __repr__(self):
         return "<sphere.Polygon %s without %s>" % (
                     self.vertices, self.external_point)
@@ -33,6 +133,14 @@ class Segment:
     def get_greatcircle(self):
         """ Returns the great circle of which this arc is a part. """
         return GreatCircle.through(self.point1, self.point2)
+    def is_endpoint(self, point):
+        return point == self.point1 or point == self.point2
+    def other_endpoint(self, endpoint):
+        """ Given an endpoint of the segment, returns the other one. """
+        if endpoint == self.point1:
+            return self.point2
+        assert endpoint == self.point2
+        return self.point1
     def contains(self, point, assume_on_greatcircle=False):
         """ Returns whether the point is contained in the segment.
 
@@ -49,6 +157,31 @@ class Segment:
         return (self.point1.distance_to(point) ** 2 +
                 self.point2.distance_to(point) ** 2
                     <= self.point1.distance_to(self.point2) ** 2)
+    def intersection(self, other):
+        """ Returns intersection-point of two segments or None if
+            the segments do not intersect. """
+        c1 = self.get_greatcircle()
+        c2 = other.get_greatcircle()
+        p = c1.intersection(c2)
+        if self.contains(p) and other.contains(p):
+            return p
+        p = -p
+        if self.contains(p) and other.contains(p):
+            return p
+        return None
+    def get_center(self):
+        """ Returns the center point of the segment. """
+        return Point(self.point1.x + self.point2.x,
+                     self.point1.y + self.point2.y,
+                     self.point1.z + self.point2.z)
+    def __eq__(self, other):
+        return ((self.point1, self.point2) == (other.point1, other.point2) or
+                (self.point1, self.point2) == (other.point2, other.point1))
+    def __ne__(self, other):
+        return ((self.point1, self.point2) != (other.point1, other.point2) and
+                (self.point1, self.point2) != (other.point2, other.point1))
+    def __repr__(self):
+        return "<sphere.Segment %s %s>" % (self.point1, self.point2)
 
 class GreatCircle:
     """ A circle that splits the sphere in two.  Equivalently: the
@@ -61,15 +194,16 @@ class GreatCircle:
     def through(point1, point2):
         """ Returns the great circle that goes through the given
             points. """
-        return GreatCircle(cross(point1, point2))
+        return GreatCircle(cross_product(point1, point2))
     def contains(self, point):
         """ Checks whether point is contained in this great circle. """
         return self.normal.orthogonal_to(point)
-    def intersect(self, other):
+    def intersection(self, other):
         """ Returns one of the two intersection point of this and the
             other great circle. """
-        assert self != other
-        return cross(self.normal, other.normal)
+        if self == other:
+            raise SameGreatCircle
+        return cross_product(self.normal, other.normal)
     def __repr__(self):
         return "<sphere.GreatCircle %s>" % self.normal
 
@@ -80,13 +214,9 @@ class GreatCircle:
 
 class Point:
     """ A point on the unit sphere. """
-    def __init__(self, x, y, z, normalize=False):
-        """ Creates a point by its 3D coordinates """
-        if normalize:
-            n = math.sqrt(x**2 + y**2 + z**2)
-            x, y, z = x/n, y/n, z/n
-        else:
-            assert x**2 + y**2 + z**2 == 1
+    def __init__(self, x, y, z):
+        """ Creates a point by its unnormalized 3D coordinates """
+        assert x != 0 or y != 0 or z != 0
         self.x, self.y, self.z = x, y, z
 
     def __str__(self):
@@ -95,29 +225,63 @@ class Point:
     def __repr__(self):
         return "<sphere.Point (%s %s %s)>" % (self.x, self.y, self.z)
 
+    def _get_norm(self):
+        """ Returns the norm of the internal representative.
+            Note: the actual norm is always 1. """
+        return math.sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
+
+    def _get_normalized_tuple(self):
+        """ Gets cartesian coordinates of the Point. """
+        norm = self._get_norm()
+        return (self.x / norm, self.y / norm, self.z / norm)
+
     def distance_to(self, other):
         """ Returns the distance to another point. """
-        return math.sqrt((self.x - other.x)**2 +
-                         (self.y - other.y)**2 +
-                         (self.z - other.z)**2)
+        norm_self = self._get_norm()
+        norm_other = other._get_norm()
+        return math.sqrt((self.x / norm_self - other.x / norm_other)**2 +
+                         (self.y / norm_self - other.y / norm_other)**2 +
+                         (self.z / norm_self - other.z / norm_other)**2)
     def orthogonal_to(self, other):
         """ Returns whether this point is orthogonal to other. """
-        return self.x * other.x + self.y * other.y + self.z * other.z == 0
+        return scalar_product(self, other) == 0
     def collinear(self, other):
         """ Checks whether this point is on the same line through the
             origin as the other point.  Or equivalently: whether this
             point is equal or antipodal to the other. """
-        return self == other or self == -other
+        return (self.x * other.y == self.y * other.x and
+                self.y * other.z == self.z * other.y)
     def __eq__(self, other):
-        return (self.x, self.y, self.z) == (other.x, other.y, other.z)
+        if not isinstance(other, Point):
+            return False
+        return (self.x * other.y == self.y * other.x and
+                self.y * other.z == self.z * other.y and
+                (self.x * other.x > 0 or
+                 self.y * other.y > 0 or
+                 self.z * other.z > 0))
     def __ne__(self, other):
-        return (self.x, self.y, self.z) != (other.x, other.y, other.z)
+        if not isinstance(other, Point):
+            return True
+        return ((self.x * other.y != self.y * other.x or
+                 self.y * other.z != self.z * other.y) or
+                (self.x * other.x < 0 or
+                 self.y * other.y < 0 or
+                 self.z * other.z < 0))
     def __neg__(self):
         return Point(-self.x, -self.y, -self.z)
+    def __hash__(self):
+        return hash(self._get_normalized_tuple())
 
-def cross(point1, point2):
-    """ Returns the normalized cross product of point1 and point2. """
+def scalar_product(point1, point2):
+    """ Returns the (unnormalized) scalar product of two points. """
+    return point1.x * point2.x + point1.y * point2.y + point1.z * point2.z
+
+def cross_product(point1, point2):
+    """ Returns the cross product of point1 and point2. """
     return Point(point1.y * point2.z - point1.z * point2.y,
                  point1.z * point2.x - point1.x * point2.z,
-                 point1.x * point2.y - point1.y * point2.x,
-                 normalize=True)
+                 point1.x * point2.y - point1.y * point2.x)
+
+def scalar_triple_product(point1, point2, point3):
+    """ Returns the scalar triple product of the three points. """
+    return scalar_product(cross_product(point1, point2),  point3)
--- src/tests/test_core.py
+++ src/tests/test_core.py
@@ -10,26 +10,37 @@ def close(x, y):
 class TestCore(unittest.TestCase):
     def setUp(self):
         self.p1 = sphere.Point(F(1), F(0), F(0))
-        self.p2 = sphere.Point(F(0), F(1), F(0))
-        self.p3 = sphere.Point(F(0), F(0), F(1))
-        self.p4 = sphere.Point(F(4, 5), F(3, 5), F(0))
-        self.p5 = sphere.Point(F(-4, 5), F(3, 5), F(0))
-        self.seg = sphere.Segment(self.p1, self.p2)
+        self.p2 = sphere.Point(F(0), F(2), F(0))
+        self.p3 = sphere.Point(F(0), F(0), F(3))
+        self.p4 = sphere.Point(F(4), F(3), F(0))
+        self.p5 = sphere.Point(F(-4), F(3), F(0))
+        self.p6 = sphere.Point(F(1), F(2), F(2))
+        self.p7 = sphere.Point(F(-1), F(2), F(2))
+        self.p8 = sphere.Point(F(1), F(-0.1), F(2))
+        self.seg1 = sphere.Segment(self.p1, self.p2)
+        self.seg2 = sphere.Segment(self.p3, self.p4)
+        self.seg3 = sphere.Segment(self.p4, self.p5)
+        self.seg4 = sphere.Segment(self.p2, self.p3)
+        self.seg5 = sphere.Segment(self.p6, self.p7)
         self.c1 = sphere.GreatCircle.through(self.p1, self.p2)
         self.c2 = sphere.GreatCircle.through(self.p1, self.p3)
         self.c3 = sphere.GreatCircle.through(self.p1, self.p4)
+        self.poly1 = sphere.Polygon([self.p3, self.p1, self.p4, self.p2],
+                                        self.p5)
     def test_point_distance(self):
         self.assertEqual(self.p1.distance_to(self.p2), math.sqrt(2))
         self.assertEqual(self.p1.distance_to(self.p1), 0)
     def test_equality(self):
         self.assertFalse(self.p1 == self.p2)
         self.assertTrue(self.p1 == self.p1)
+        self.assertTrue(self.p1 != self.p2)
+        self.assertFalse(self.p1 != self.p1)
     def test_segment(self):
-        self.assertTrue(close(self.seg.get_arc(), math.pi / 2))
+        self.assertTrue(close(self.seg1.get_arc(), math.pi / 2))
     def test_cross(self):
-        self.assertEqual(sphere.cross(self.p1, self.p2), self.p3)
-        self.assertEqual(sphere.cross(self.p2, self.p1), -self.p3)
-        self.assertEqual(sphere.cross(self.p1, self.p4), self.p3)
+        self.assertEqual(sphere.cross_product(self.p1, self.p2), self.p3)
+        self.assertEqual(sphere.cross_product(self.p2, self.p1), -self.p3)
+        self.assertEqual(sphere.cross_product(self.p1, self.p4), self.p3)
     def test_orthogonal(self):
         self.assertTrue(self.p1.orthogonal_to(self.p2))
         self.assertTrue(self.p1.orthogonal_to(self.p3))
@@ -46,14 +57,26 @@ class TestCore(unittest.TestCase):
         self.assertFalse(self.c1 == self.c2)
         self.assertFalse(self.c1 != self.c3)
         self.assertTrue(self.c1 == self.c3)
-        self.assertTrue(self.c1.intersect(self.c2).collinear(self.p1))
+        self.assertTrue(self.c1.intersection(self.c2).collinear(self.p1))
     def test_segment(self):
-        self.assertTrue(self.seg.contains(self.p1))
-        self.assertTrue(self.seg.contains(self.p2))
-        self.assertTrue(self.seg.contains(self.p4))
-        self.assertFalse(self.seg.contains(self.p5))
-        self.assertFalse(self.seg.contains(self.p5, True))
-        self.assertFalse(self.seg.contains(self.p5))
+        self.assertTrue(self.seg1.contains(self.p1))
+        self.assertTrue(self.seg1.contains(self.p2))
+        self.assertTrue(self.seg1.contains(self.p4))
+        self.assertFalse(self.seg1.contains(self.p5))
+        self.assertFalse(self.seg1.contains(self.p5, True))
+        self.assertFalse(self.seg1.contains(self.p5))
+    def test_segment_intersection(self):
+        self.assertTrue(self.seg5.intersection(self.seg4))
+        self.assertTrue(self.seg1.intersection(self.seg2))
+        self.assertTrue(self.seg1.intersection(self.seg5) is None)
+        self.assertTrue(self.seg1.intersection(self.seg2))
+    def test_polygon_contains(self):
+        # TODO add tests for all cornercases
+        self.assertTrue(self.poly1.contains(self.p6))
+        self.assertFalse(self.poly1.contains(self.p5))
+        self.assertFalse(self.poly1.contains(self.p7))
+        self.assertFalse(self.poly1.contains(-self.p5))
+        self.assertFalse(self.poly1.contains(self.p8))
 
 if __name__ == '__main__':
     unittest.main()
